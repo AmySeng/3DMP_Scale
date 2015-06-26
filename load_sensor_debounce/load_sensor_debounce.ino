8600b166c9438b034dfc336ebe93f765e0a513e7
Jonathan Ota <jonathanota1@gmail.com>
2015-06-26 10:57:07 -0700

    init commit
    debouncing scale. threshold values can still change, it’s still
    sampling at a decently high rate.

---
+++ load_sensor_debounce/load_sensor_debounce.ino
@@ -0,0 +1,47 @@
+#include "HX711.h"
+
+HX711 scale(A2, A3);
+
+float totalWeight, previousTotalWeight, measuredWeight;
+float pickedUpThresh = -0.1;
+boolean pickedUp = true;
+
+long lastDebounceTime = 0;
+long debounceDelay = 500;
+
+void setup() {
+
+  Serial.begin(9600);
+  //---- Scale setup ---//
+  scale.read();
+  scale.set_scale(2280.f);
+  scale.tare();  
+
+}
+
+void loop() {
+   
+   totalWeight = scale.get_units(5),1;
+   Serial.println(totalWeight);
+   
+   if (totalWeight >= previousTotalWeight + 1.0 || 
+        totalWeight <= previousTotalWeight - 1.0){
+    lastDebounceTime = millis();
+    Serial.println("different weight");
+   }
+
+   if (millis() - lastDebounceTime > debounceDelay){
+    measuredWeight = totalWeight;
+    Serial.println("settled weight measured");
+//    Serial.println(measuredWeight);
+   }
+
+
+
+
+
+
+
+   previousTotalWeight = totalWeight;
+
+}
