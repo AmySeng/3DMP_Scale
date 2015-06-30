7c320b9a73a320e45f163e080337c56fa9eb61bb
AmySeng <amy.seng@gravitytank.com>
2015-06-30 08:32:41 -0700

    brute force for sets of 2
    it works!

---
+++ load_sensor_v1.1/brute_force/brute_force.ino
@@ -0,0 +1,41 @@
+#include <HashMap.h>
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  byte products[] = {
+    1, 1, 1, 3, 3, 3, 7, 7, 15, 15, 31, 31, 31
+  };
+  byte product_count = 13;
+
+  HashType<byte, char*> hashRawArray[77];
+  HashMap<byte, char*> lookup = HashMap<byte, char*>(hashRawArray, 77);
+
+  byte sum = 0;
+  byte lookup_cnt = 0;
+
+  char subset[77][10];
+  
+  for (byte i=0; i < product_count-1; i++) {
+    for (byte j=i+1; j < product_count; j++) {
+      sum = products[i] + products[j];
+      String middle = ", ";
+      String subset_tmp = products[i] + middle + products[j];
+      subset_tmp.toCharArray(subset[lookup_cnt], 10);
+      lookup[lookup_cnt](sum, subset[lookup_cnt]);
+      lookup_cnt++;
+    }
+  }
+  
+  // FIND Subset
+  byte weight = 46;
+  Serial.print("Looking for: ");
+  Serial.println(weight);
+  Serial.println(lookup.getValueOf(weight));
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
--- load_sensor_v1.1/which_products_v2/which_products_v2.ino
+++ load_sensor_v1.1/which_products_v2/which_products_v2.ino
@@ -1,19 +1,18 @@
 #include <HashMap.h>
 
-int products[] = {
-  2, 2, 2, 4, 4, 4, 8, 8, 16, 16, 32, 32, 32
+byte products[] = {
+  1, 1, 1, 3, 3, 3, 7, 7, 15, 15, 31, 31, 31
 };
-const int PRODUCT_COUNT = 13;
-const int MAX_SUM = 162;
-const int SUM_RANGE = MAX_SUM-2+1;
-int sum_range = 0;
-int prod_index = 0;
+const byte PRODUCT_COUNT = 13;
+const byte MAX_SUM = 62;
+const byte SUM_RANGE = MAX_SUM-1+1;
+//int sum_range = 0;
+//int prod_index = 0;
 
-//HashType<char*, int> hashRawArray[SUM_RANGE];
-//HashMap<char*, int> sum_lookup = HashMap<char*, int>(hashRawArray, SUM_RANGE);
+HashType<byte, byte> hashRawArray[SUM_RANGE];
+HashMap<byte, byte> sum_lookup = HashMap<byte, byte>(hashRawArray, SUM_RANGE);
 
-int sum_lookup[100];
-int matrix[PRODUCT_COUNT+1][SUM_RANGE];
+byte matrix[PRODUCT_COUNT+1][SUM_RANGE];
 
 void setup() {
   // put your setup code here, to run once:
@@ -26,10 +25,10 @@ void setup() {
   fill_first_row();
   
   fill_matrix();
-  // print_matrix();
+  print_matrix();
   
   int subset[PRODUCT_COUNT] = {0};
-  int Weight = 20;
+  int Weight = 46;
   find_subset(subset, Weight);
 
   Serial.print("LOOKING FOR... ");
@@ -55,44 +54,37 @@ void initiate_matrix() {
 
   // determine smallest and largest sums
   for (i = 0; i < PRODUCT_COUNT; i++) {
-    max_sum += products[i];
+    // max_sum += products[i];
     if (products[i] < smallest) {
       smallest = products[i];
     }
   }
     
   // create first row of matrix
-  sum_range = max_sum - smallest + 1;
-  for (i = 0; i < sum_range; i++) {
+  for (i = 0; i < SUM_RANGE; i++) {
     matrix[0][i] = smallest + i;
   }
 
   // build lookup for sums
-  for (i=0; i < 100; i++) {
-      sum_lookup[i] = -1;
-  }
-  for (i = 0; i < sum_range; i++) {
-    sum_lookup[matrix[0][i]] = i;
+  for (i = 0; i < SUM_RANGE; i++) {
+    sum_lookup[i](matrix[0][i], i);
   }
 
   // set all to False
-  for (prod_index = 1; prod_index <= PRODUCT_COUNT; prod_index++) {
-    for (i = 0; i < sum_range; i++) {
+  for (int prod_index = 1; prod_index <= PRODUCT_COUNT; prod_index++) {
+    for (i = 0; i < SUM_RANGE; i++) {
       matrix[prod_index][i] = 'F';
     }
   }
 }
 
 int find_column_index(int sum) {
-  if (sum_lookup[sum] < sum_range && sum_lookup[sum] >= 0) {
-     return sum_lookup[sum];
-  }
-  return -1;
+  return sum_lookup.getValueOf(sum);
 }
 
 void fill_first_row() {
   int i;
-  for (i = 0; i < sum_range; i++) {
+  for (i = 0; i < SUM_RANGE; i++) {
     if (products[0] == matrix[0][i]) {
       matrix[1][i] = 'T';
     }
@@ -103,8 +95,8 @@ void fill_matrix() {
   int i = 0;
   int row;
   for (row = 2; row <= PRODUCT_COUNT; row++) {
-    prod_index = row - 1;
-    for (i = 0; i < sum_range; i++) {
+    int prod_index = row - 1;
+    for (i = 0; i < SUM_RANGE; i++) {
       if (products[prod_index] == matrix[0][i] ||
           matrix[row - 1][i] == 'T' ||
           current_sum_possible(row, prod_index, i)) {
@@ -117,11 +109,12 @@ void fill_matrix() {
 bool current_sum_possible(int tmp_row, int tmp_index, int j) {
   int column_sum = matrix[0][j] - products[tmp_index];
   int column_index = find_column_index(column_sum);
-  if (column_index == -1) return false;
+  //if (column_index == -1) return false;
   
   if (matrix[tmp_row - 1][column_index] == 'T') {
     return true;
   }
+  
   else return false;
 }
 
@@ -131,7 +124,6 @@ void find_subset(int subset[], int sum) {
   int column_index = find_column_index(sum);
   int tmp_sum;
   for (row = PRODUCT_COUNT; row > 0; row--) {
-    if (column_index == -1) return;
     if (matrix[row][column_index] == 'F') {
       return;
     }
@@ -151,16 +143,15 @@ void find_subset(int subset[], int sum) {
 void print_matrix() {
   int i=0;
   int tmp=0;
-  Serial.print("MATRIX");
-  Serial.println();
-  for (i=0; i < 31; i++) {
+  Serial.println("MATRIX");
+  for (i=0; i < SUM_RANGE; i++) {
     Serial.print(matrix[0][i]);
     Serial.print(" ");
   }
   Serial.println();
 
   for (tmp=1; tmp <= PRODUCT_COUNT; tmp++) {
-    for(i=0; i <31; i++) {
+    for(i=0; i < SUM_RANGE; i++) {
       Serial.print(char(matrix[tmp][i]));
       Serial.print(" ");
     }
